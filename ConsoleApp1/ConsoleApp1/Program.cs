7ba0feca4fe737867f9e9ea5356bbc81e1197e68
valesja15 <valesja15@sps-prosek.cz>
2017-09-21 12:21:05 +0200

    Update 1//21/9

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,95 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("xd");
+            int sz = 0;
+            int xint = 0;
+            int yint = 0;
+
+            long[,] arr = new long[9, 9];
+
+            for(int i = 0; i < 9; i++)
+            {
+                arr[sz, sz] = 0;
+                sz = sz + 1;
+            }
+
+            int rowLength = arr.GetLength(0);
+            int colLength = arr.GetLength(1);
+
+            void map() {
+                for (int i = 0; i < rowLength; i++)
+                {
+                    for (int j = 0; j < colLength; j++)
+                    {
+                        Console.Write(string.Format("{0} ", arr[i, j]));
+                    }
+                    Console.Write(Environment.NewLine + Environment.NewLine);
+                }
+            }
+
+            void text()
+            {
+                Console.WriteLine("\nZadej pozici, kterou chceš napadnout\n");
+                
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("X = ");
+                        string x = Console.ReadLine();
+                        xint = int.Parse(x);
+                        xint = xint - 1;
+                        if (xint > 8 || xint < 0)
+                        {
+                            Console.WriteLine("Pouze v rozmezí 1 - 9");
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Pouze čísla");
+                    }
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Y = ");
+                        string y = Console.ReadLine();
+                        yint = int.Parse(y);
+                        yint = yint - 1;
+                        if (yint > 8 || yint < 0)
+                        {
+                            Console.WriteLine("Pouze v rozmezí 1 - 9");
+                            continue;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Pouze čísla\n");
+                    }
+                }
+
+                arr[xint, yint] = 1;
+            }
+
+            for(int i = 0; i <= 81; i++)
+            {
+                map();
+                text();
+            }
+
+            Console.WriteLine();
         }
     }
 }
