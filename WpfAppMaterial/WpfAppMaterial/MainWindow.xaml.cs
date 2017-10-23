a163f8da34d98aa00009e117380f7a97aa60d8d5
Honza <Honza@KLEVCX-PC>
2017-10-23 22:23:24 +0200

    Semi-Final

--- WpfAppMaterial/WpfAppMaterial/MainWindow.xaml.cs
+++ WpfAppMaterial/WpfAppMaterial/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,10 +29,31 @@ namespace WpfAppMaterial
         public MainWindow()
         {
             InitializeComponent();
-            int rand = random.Next(0, 10);
         }
         public void answ1_Click(object sender, RoutedEventArgs e)
         {
+
+            if (result.Equals(answ1.Content))
+            {
+                proBar.Value = progress = progress + 10;
+            }
+            else
+            {
+                proBar.Value = progress = progress - 10;
+            }
+            try
+            {
+                if ((string)answ1.Content == "Konec")
+                {
+                    Environment.Exit(0);
+                }
+            } catch(Exception)
+            {
+
+            }
+
+            int rand = random.Next(0, 10);
+
             int num1 = random.Next(0, 100);
             int num2 = random.Next(0, 100);
             result = num1 + num2;
@@ -42,18 +64,18 @@ namespace WpfAppMaterial
             {
                 answ1.Content = num1 + num2;
                 answ2.Content = num2 * 2;
-            //UPRAVIT    proBar.Value = progress = progress + 10;
             } else
             {
-                answ1.Content = num1 * 2;
+                answ1.Content = num1 *2;
                 answ2.Content = num1 + num2;
-            //UPRAVIT    proBar.Value = progress = progress - 10;
             }
-
 
             if (proBar.Value >= 100)
             {
                 progress = 100;
+                question.Text = "Vyhrál jsi!";
+                answ1.Content = "Konec";
+                answ2.Content = "Konec";
             }
             else if (proBar.Value <= 0)
             {
@@ -63,6 +85,28 @@ namespace WpfAppMaterial
 
         private void answ2_Click(object sender, RoutedEventArgs e)
         {
+
+            if (result.Equals(answ2.Content))
+            {
+                proBar.Value = progress = progress + 10;
+            }
+            else
+            {
+                proBar.Value = progress = progress - 10;
+            }
+            try
+            {
+                if ((string)answ2.Content == "Znovu")
+                {
+                    Environment.Exit(0);
+                }
+            } catch(Exception)
+            {
+
+            }
+
+            int rand = random.Next(0, 10);
+
             int num1 = random.Next(0, 100);
             int num2 = random.Next(0, 100);
             result = num1 + num2;
@@ -71,29 +115,30 @@ namespace WpfAppMaterial
 
             question.Text = questions;
 
-            if (rand % 2 == 1)
+            if (rand % 2 != 0)
             {
                 answ2.Content = num1 + num2;
                 answ1.Content = num1 * 2;
-            //UPRAVIT    proBar.Value = progress = progress + 10;
             }
             else
             {
                 answ2.Content = num1 * 2;
                 answ1.Content = num1 + num2;
-            //UPRAVIT    proBar.Value = progress = progress - 10;
 
             }
-
 
             if (proBar.Value >= 100)
             {
                 progress = 100;
+                question.Text = "Vyhrál jsi!";
+                answ1.Content = "Konec";
+                answ2.Content = "Konec";
             }
             else if (proBar.Value <= 0)
             {
                 progress = 0;
-            };
+            }
+
         }
     }
 
