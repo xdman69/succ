d914e5d834b4307127b358083d83aba799155151
valesja15 <valesja15@sps-prosek.cz>
2017-11-21 10:40:27 +0100

    PalindromCheck

---
+++ XMA1/XMA1/XMA1/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Xamarin.Forms;
+
+namespace XMA1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new XMA1.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XMA1/XMA1/XMA1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace XMA1
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<string> Items = new ObservableCollection<string>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+  
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            string text = entryValue.Text;
+            string nospace = System.Text.RegularExpressions.Regex.Replace(text, @"\s+", "").ToLower().Replace(",","").Replace("?", "").Replace(".","").Replace("!","");
+            char[] rev = nospace.ToCharArray();
+            Array.Reverse(rev);
+            string revs = new string(rev);
+
+            if (nospace.Equals(revs))
+            {
+                Items.Add(text);
+                nameList.ItemsSource = Items;
+                check.Text = text + " je Palindrom";
+                check.TextColor = Color.DarkGreen;
+            } else
+            {
+                check.Text = text + " není Palindrom";
+                check.TextColor = Color.DarkRed;
+            }
+        }
+    }
+}
