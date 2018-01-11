726a30a6f71933442d09f4b490edad1e8769f7d0
valesja15 <valesja15@DST20708>
2018-01-11 12:32:08 +0100

    mediaplayer

---
+++ MediaElement/MediaElement/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MediaElement
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool userIsDraggingSlider = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (playButton.Content.ToString() == "▶")
+            {
+                mediaPlayer.Play();
+                playButton.Content = "◼";
+            }
+            else
+            {
+                mediaPlayer.Pause();
+                playButton.Content = "▶";
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if ((mediaPlayer.Source != null) && (mediaPlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            {
+                sliProgress.Minimum = 0;
+                sliProgress.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliProgress.Value = mediaPlayer.Position.TotalSeconds;
+            }
+        }
+
+
+        private void forwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Position += TimeSpan.FromSeconds(10);
+        }
+
+        private void backwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Position -= TimeSpan.FromSeconds(10);
+        }
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Videos";
+            dialog.DefaultExt = ".WMV";
+            dialog.Filter = "WMV file (.wm)|*.wmv";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                mediaPlayer.Source = new Uri(dialog.FileName);
+                playButton.IsEnabled = true;
+            }
+        }
+
+        private void volumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaPlayer.Volume = volumeSlider.Value;
+        }
+
+        private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            mediaPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+        }
+
+        private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TimeStamp.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+
+        }
+    }
+}
