844e09b4349f681bb191285ed9f1e79b650be7fd
valesja15 <valesja15@DST20708>
2018-01-18 10:20:42 +0100

    icons

--- ToDoList/ToDoList/ToDoList/ToDoItem.cs
+++ ToDoList/ToDoList/ToDoList/ToDoItem.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using SQLite;
-using SQLitePCL;
 
-namespace ToDoList
+namespace SQLiteExample
 {
-    class ToDoItem
+    public class TodoItem
     {
-        public class TodoItem
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+        public TodoItem()
         {
-            [PrimaryKey, AutoIncrement]
-            public int ID { get; set; }
-            public string Name { get; set; }
-            public string Text { get; set; }
-            public TodoItem()
-            {
-            }
-            public override string ToString()
-            {
-                return "ID" + ID + " Name " + Name + " Text " + Text;
-            }
+        }
+
+        public override string ToString()
+        {
+            return "ID" + ID + " Name " + Name + " Text " + Text;
         }
     }
 }
