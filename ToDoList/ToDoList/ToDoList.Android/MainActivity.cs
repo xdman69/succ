c8a00b9287f33bf9a77d56a94712297f51d4f03b
valesja15 <valesja15@DST20708>
2018-01-18 11:04:50 +0100

    so

--- ToDoList/ToDoList/ToDoList.Android/MainActivity.cs
+++ ToDoList/ToDoList/ToDoList.Android/MainActivity.cs
@@ -6,10 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using ToDoList.Droid;
 
-namespace ToDoList.Droid
+namespace SQLiteExample.Droid
 {
-    [Activity(Label = "ToDoList", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SQLiteExample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -24,4 +25,3 @@ namespace ToDoList.Droid
         }
     }
 }
-
--- ToDoList/ToDoList/ToDoList/MainPage.xaml.cs
+++ ToDoList/ToDoList/ToDoList/MainPage.xaml.cs
@@ -1,28 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 
-namespace ToDoList
+namespace SQLiteExample
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-            FillList();
-        }
-
-        public void FillList()
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
         {
-            List<string> list = new List<string>()
+            InitializeComponent();
+            var dbConnection = App.Database;
+
+            TodoItemDatabase todoItemDatabase = App.Database;
+            TodoItem item = new TodoItem();
+            item.Name = "item";
+            item.Text = "item text";
+            App.Database.SaveItemAsync(item);
+
+
+            var itemsFromDb = App.Database.GetItemsAsync().Result;
+
+            Debug.WriteLine("                             ");
+            Debug.WriteLine("                             ");
+            Debug.WriteLine("                             ");
+
+            Debug.WriteLine(itemsFromDb.Count);
+            foreach (TodoItem todoItem in itemsFromDb)
             {
-                "NAKUP CHLEBÍK", "NAKUP KEDLUBEN", "JDI VRÁTIT FLAŠKY"
-            };
-            listToDo.ItemsSource = list;
+                Debug.WriteLine(todoItem);
+            }
+
+            Debug.WriteLine("                             ");
+            Debug.WriteLine("                             ");
+            Debug.WriteLine("                             ");
 
         }
-	}
+    }
 }
